928de2d5c34730e67530ec85219299f5759a1595
steve-the-hermit <stevemacharia309@gmail.com>
2023-09-16 13:27:03 +0300

    done

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
-
 from user import User
 
-import random
-
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Math", "Science", "History"]
 
     def teach(self):
-        pass
+        return "Math"  # You can change this to teach different subjects.
